4e293db15ba3f83785cd22d71f63ffab29f51318
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-05-23 15:16:40 +0800

    Add basic file

---
+++ main/main.c
@@ -0,0 +1,119 @@
+/*
+ * RB-Tree Manual.
+ *
+ * (C) 2019.05.14 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* rbtree */
+#include <linux/rbtree.h>
+
+struct node {
+	struct rb_node node;
+	unsigned long runtime;
+};
+
+/*
+ * RB-Tree
+ *
+ *                                                        [] Black node
+ *                                                        () Red node
+ *                    [4]
+ *                     |
+ *          o----------o----------o
+ *          |                     |
+ *         (2)                   (7)
+ *          |                     |
+ *   o------o------o      o-------o-------o
+ *   |             |      |               |             
+ *  [1]           [3]    [5]             [9]
+ *                                        |
+ *                                o-------o-------o
+ *                                |               |
+ *                               (8)            (129)
+ *                      
+ *
+ */
+static struct node node0 = { .runtime = 0x1 };
+static struct node node1 = { .runtime = 0x2 };
+static struct node node2 = { .runtime = 0x3 };
+static struct node node3 = { .runtime = 0x5 };
+static struct node node4 = { .runtime = 0x4 };
+static struct node node5 = { .runtime = 0x7 };
+static struct node node6 = { .runtime = 0x8 };
+static struct node node7 = { .runtime = 0x9 };
+static struct node node8 = { .runtime = 0x129 };
+
+/* rbroot */
+static struct rb_root BiscuitOS_rb = RB_ROOT;
+
+/* Insert private node into rbtree */
+static int rbtree_insert(struct rb_root *root, struct node *node)
+{
+	struct rb_node **new = &(root->rb_node), *parent = NULL;
+
+	/* Figure out where to put new node */
+	while (*new) {
+		struct node *this = rb_entry(*new, struct node, node);
+		int result;
+
+		/* Compare runtime */
+		result = this->runtime - node->runtime;
+
+		/* setup parent */
+		parent = *new;
+
+		/*
+		 *        (this)
+		 *         /  \
+		 *        /    \
+		 *  (little)   (big)
+		 *
+		 */
+		if (result < 0)
+			new = &((*new)->rb_right);
+		else if (result > 0)
+			new = &((*new)->rb_left);
+		else
+			return 0;
+	}
+
+	/* Add new node and rebalance tree */
+	rb_link_node(&node->node, parent, new);
+	rb_insert_color(&node->node, root);
+	return 0;
+}
+
+int main(void)
+{
+	struct node *np, *n;
+	struct rb_node *node;
+
+	/* Insert rb_node */
+	rbtree_insert(&BiscuitOS_rb, &node0);
+	rbtree_insert(&BiscuitOS_rb, &node1);
+	rbtree_insert(&BiscuitOS_rb, &node2);
+	rbtree_insert(&BiscuitOS_rb, &node3);
+	rbtree_insert(&BiscuitOS_rb, &node4);
+	rbtree_insert(&BiscuitOS_rb, &node5);
+	rbtree_insert(&BiscuitOS_rb, &node6);
+	rbtree_insert(&BiscuitOS_rb, &node7);
+	rbtree_insert(&BiscuitOS_rb, &node8);
+
+	printf("Iterate over RBTree.\n");
+	for (node = rb_first(&BiscuitOS_rb); node; node = rb_next(node))
+		printf("%#lx ", rb_entry(node, struct node, node)->runtime);
+	printf("\n");
+
+	printf("Iterate over by postorder.\n");
+	rbtree_postorder_for_each_entry_safe(np, n, &BiscuitOS_rb, node)
+		printf("%#lx ", np->runtime);
+	printf("\n");
+	
+	return 0;
+}
